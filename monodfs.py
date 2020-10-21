53c54dc801399a6d425e7ac9ef6e25c1b86964fd
John Lewis <johlewis@pdx.edu>
2020-10-21 16:54:05 -0700

    set up problem variables

--- monodfs.py
+++ monodfs.py
@@ -3,7 +3,21 @@
 import sys
 
 def main():
-  print(len(sys.argv), "arguments:", str(sys.argv))
+
+  #Return  -1 if too few args
+  if(len(sys.argv) < 3):
+    print("Incorrect Number of Arguments\n")
+    return  -1;
+
+  m = int(sys.argv[1])   # Upper limit of {1 ... m} monopoles
+  n = int(sys.argv[2])   # Number of rooms to sort into
+
+  rooms = []             # List of monopole sets for each room
+  monopoles = set((range(1, m+1)))    # Set of all monopoles
+
+  print (monopoles)
+
+
 
 if __name__ == "__main__":
   main()
